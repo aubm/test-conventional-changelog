aca72bbcd304379433e863ecf6a83a29d691d5d6
kendo5731 <aur.baumann@gmail.com>
2015-10-23 10:42:09 +0200

    refactor(changelog): use gulp-conventional-changelog

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require("gulp");
 var util = require("gulp-util");
 var fs = require("fs");
-var conventionalChangelog = require("conventional-changelog");
+var conventionalChangelog = require("gulp-conventional-changelog");
 
 var argv = util.env;
 
 var releaseCount = !!argv.env ? argv.env : '1';
 
 gulp.task("changelog", function() {
-    return conventionalChangelog({
-        preset: 'angular',
-        releaseCount: releaseCount
-    })
-        .pipe(fs.createWriteStream('CHANGELOG.md'));
+    return gulp.src("CHANGELOG.md", { buffer: false })
+        .pipe(conventionalChangelog({
+            preset: 'angular',
+            releaseCount: releaseCount
+        }))
+        .pipe(gulp.dest("./"));
 });
