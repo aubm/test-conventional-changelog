23a5359aa9dee6ddca9a02ab5e23704a7c7c8146
kendo5731 <aur.baumann@gmail.com>
2015-10-23 10:34:15 +0200

    feat(changelog): add release count argument

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require("gulp");
+var util = require("gulp-util");
 var fs = require("fs");
 var conventionalChangelog = require("conventional-changelog");
 
+var argv = util.argv;
+
+var releaseCount = !!argv.env ? argv.env : '1';
+
 gulp.task("changelog", function() {
-    return conventionalChangelog({ preset: 'angular' })
+    return conventionalChangelog({
+        preset: 'angular',
+        releaseCount: releaseCount
+    })
         .pipe(fs.createWriteStream('CHANGELOG.md'));
 });
